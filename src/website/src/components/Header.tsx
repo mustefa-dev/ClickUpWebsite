a2a5ff968ac7ddcab29179f2cd1f2757771b60ba
Mustafa Hassan <cs.21.065@student.uotechnology.edu.iq>
2024-08-29 15:35:47 +0300

    user section

--- src/website/src/components/Header.tsx
+++ src/website/src/components/Header.tsx
@@ -21,7 +21,7 @@ function Header({ isSidebarOpen, toggleSidebar }: HeaderProps) {
     return (
         <nav className="fixed top-0 left-0 right-0 z-50 p-4 bg-secondary text-secondary-foreground shadow-sm custom-border">
             <div className="flex items-center justify-between">
-                <p className="text-xl text-primary ml-4">Buildify.io</p>
+                <p className="text-xl text-primary ml-4">الدعم الفني</p>
                 <div className={`flex items-center transition-transform duration-300 ${isSidebarOpen ? "translate-x-[250px]" : ""}`}>
                     {/* Removed the <p> element */}
                 </div>
