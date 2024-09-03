650382469fc122159afb111afef236360469d1d4
Mustafa Hassan <cs.21.065@student.uotechnology.edu.iq>
2024-09-03 18:14:29 +0300

    SignalR clientSide

--- src/website/src/App.tsx
+++ src/website/src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import * as signalR from "@microsoft/signalr";
 
 import { ThemeProvider } from "./context/theme-provider";
 import { AuthStore } from "@/utils/authStore";
@@ -20,6 +19,40 @@ function App() {
         }
         console.log(isLogin);
     }, [isLogin]);
+
+    useEffect(() => {
+        const requestNotificationPermission = async () => {
+            if (Notification.permission !== "granted") {
+                await Notification.requestPermission();
+            }
+        };
+
+        requestNotificationPermission();
+
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl("http://localhost:5194/ticketsHub", { withCredentials: true })
+            .build();
+
+        connection.start()
+            .then(() => {
+                console.log("Connected to the SignalR hub");
+            })
+            .catch(err => {
+                console.error("Error connecting to the SignalR hub:", err);
+            });
+
+        connection.on("NewTicket", (ticket) => {
+            if (Notification.permission === "granted") {
+                new Notification("New Ticket", {
+                    body: JSON.stringify(ticket),
+                });
+            }
+        });
+
+        return () => {
+            connection.stop().then(() => console.log("Disconnected from the SignalR hub"));
+        };
+    }, []);
 
     const client = new QueryClient({
         defaultOptions: {
@@ -46,10 +79,8 @@ function App() {
                         </BrowserRouter>
                     </div>
                 </ThemeProvider>
-                <ToastContainer />
             </QueryClientProvider>
         </>
     );
 }
-
 export default App;
