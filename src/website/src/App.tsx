051bfbbda52c0e634b1dad99c870d8f5df9c4ffc
Mustafa Hassan <cs.21.065@student.uotechnology.edu.iq>
2024-09-05 19:14:13 +0300

    multiple users ..

--- src/website/src/App.tsx
+++ src/website/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import * as signalR from "@microsoft/signalr";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "./context/theme-provider";
@@ -11,45 +10,6 @@ import NotificationComponent from "@/components/NotificationComponent";
 function App() {
     const [connection, setConnection] = useState(null);
     const [isLogin, setIslogin] = useState(AuthStore.getAccessToken());
-
-    useEffect(() => {
-        const userInfo = AuthStore.State.getState().userInfo;
-        setIslogin(userInfo);
-
-        if (userInfo) {
-            const newConnection = new signalR.HubConnectionBuilder()
-                .withUrl("http://localhost:5194/ticketsHub", { withCredentials: true })
-                .build();
-
-            newConnection.start()
-                .then(() => {
-                    console.log("Connected to the SignalR hub");
-
-                    if (userInfo.id) {
-                        newConnection.invoke("JoinGroup", userInfo.id);
-                    }
-
-                    setConnection(newConnection);
-                })
-                .catch(err => {
-                    console.error("Error connecting to the SignalR hub:", err);
-                });
-
-            newConnection.on("NewTicket", (ticket) => {
-                if (Notification.permission === "granted") {
-                    new Notification("New Ticket", {
-                        body: JSON.stringify(ticket),
-                    });
-                }
-            });
-        }
-
-        return () => {
-            if (connection) {
-                connection.stop().then(() => console.log("Disconnected from the SignalR hub"));
-            }
-        };
-    }, [isLogin]);
 
     const client = new QueryClient({
         defaultOptions: {
